3059b625851c6c96656b1094d2ca46e3a4bbcf77
Marc <markitusplanas@alumnes.ilerna.com>
2024-10-25 15:49:39 +0200

    input color

--- pagina4.php
+++ pagina4.php
@@ -1,6 +1,10 @@
 <?php
     include("includes/header.html");
 ?>
+    <h1>Tria color</h1>
+    <section>
+        <input type="color" id="colorInput">
+    </section>
 <?php
     include("includes/footer.html");
 ?>
