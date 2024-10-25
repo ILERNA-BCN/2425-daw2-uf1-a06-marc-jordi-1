5caaa4342f13af490a5a572e742d23e181eb1b25
Marc <markitusplanas@alumnes.ilerna.com>
2024-10-25 16:08:32 +0200

    radio inputs

--- pagina4.php
+++ pagina4.php
@@ -2,6 +2,10 @@
     include("includes/header.html");
 ?>
     <h1>Tria color</h1>
+    <input type="radio" id="color1">
+    <input type="radio" id="color2">
+    <input type="radio" id="color3">
+    <input type="radio" id="color4">
     <input type="color" id="colorInput">
 <?php
     include("includes/footer.html");
