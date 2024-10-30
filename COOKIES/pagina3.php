83b08705af049635e47ff0b016bb34850437cde1
Jordi <jordi.jbf15@alumnes.ilerna.com>
2024-10-30 17:49:50 +0100

    pagina 5 afegida

--- COOKIES/pagina3.php
+++ COOKIES/pagina3.php
@@ -9,7 +9,7 @@
     </style>
 <body>
     <nav>
-        <a href="pagina1.php">Pàgina 1</a> <a href="pagina2.php">Pàgina 2</a> <a href="pagina3.php">Pàgina 3</a> <a href="pagina4.php">Pàgina 4</a>
+    <a href="pagina1.php">Pàgina 1</a> <a href="pagina2.php">Pàgina 2</a> <a href="pagina3.php">Pàgina 3</a> <a href="pagina4.php">Pàgina 4</a>   <a href="pagina5.php">Pàgina 5</a>
     </nav>
     <br/><br/>
     <h1>PÀGINA 3</h1>
